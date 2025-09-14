f2a9ce13d1bd0615b920c841fa3afe579cbd52dd
minsoriu <kwizzsisters@gmail.com>
2025-09-14 23:34:51 +0300

    Add files via upload

---
+++ lab3.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите конечное число:");
+int x = Convert.ToInt32(Console.ReadLine());
+double s = 0;
+for (double i = 1;  i <= x; i++)
+{
+    s += 1 / i;
+}
+Console.WriteLine(s);
